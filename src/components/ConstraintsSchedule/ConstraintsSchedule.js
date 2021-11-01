592f72bae97c72d0c42eab0f3377bfb51a95084a
michal4285 <michalprober@gmail.com>
2021-11-01 22:21:33 +0200

    add ConstraintsSchedule component

---
+++ src/components/ConstraintsSchedule/ConstraintsSchedule.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState ,Component} from 'react';
+import './ConstraintsSchedule.css';
+import { connect } from 'react-redux';
+import API from '../../config/env/local'
+
+
+export default function ConstraintsSchedule() {
+
+    
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
