767cfc49743a126e3906faea20b5caffd3afa26a
michal4285 <michalprober@gmail.com>
2022-02-15 02:04:23 +0200

    add shift schedule and icon and title to the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Routes from './routes/routes';
 import NavBar from './components/Navbar/Navbar'
+import React, { useEffect } from 'react'
 
 function App() {
+
+  useEffect(() => {
+    document.title = "נותנים ידים"
+  }, [])
+
   return (
     <>
       {/* <NavBar /> */}
